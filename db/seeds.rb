3a93afa3cc285342e137195b4cba875332a767c9
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2023-04-22 23:31:25 +0800

    creates seed file for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+Product.destroy_all
+Store.destroy_all
+
+def generate_sku
+  random_string = (0...8).map { (65 + rand(26)).chr }.join + rand(1000..9999).to_s
+  sku = "SKU-#{random_string}"
+
+  sku
+end
+
+def random_quantity
+  rand(1..50)
+end
+
+def random_store_id
+  rand(20..40)
+end
+
+10.times do
+  Store.create(name: FFaker::Company.unique.name, url: FFaker::Internet.unique.http_url)
+end
+
+store_ids = Store.pluck(:id)
+
+50.times do
+  Product.create(
+    name: FFaker::Product.unique.product_name,
+    sku: generate_sku,
+    inventory_quantity: random_quantity,
+    store_id: store_ids.sample
+  )
+end

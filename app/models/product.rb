6de30242c4f1f67b43f824496a3964fbf9b361cd
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2023-04-22 23:44:33 +0800

    updates inventory update time if inventory quanity changed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
+  before_save :update_inventory_updated_time, if: :inventory_quantity_changed?
+
   belongs_to :store
+
+  def update_inventory_updated_time
+    self.inventory_updated_time = Time.now
+  end
 end
